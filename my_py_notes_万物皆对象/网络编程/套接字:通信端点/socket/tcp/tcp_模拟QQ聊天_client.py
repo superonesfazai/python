836b78754c8260518740964030cec81591dd34e6
superonesfazai <superonesfazai@gmail.com>
2017-08-13 20:21:23 +0800

    2017.8.13 commit

--- my_py_notes_万物皆对象/网络编程/套接字:通信端点/socket/tcp/tcp_模拟QQ聊天_client.py
+++ my_py_notes_万物皆对象/网络编程/套接字:通信端点/socket/tcp/tcp_模拟QQ聊天_client.py
@@ -9,27 +9,21 @@
 
 import socket
 
-# 创建socket
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# 链接服务器
-server_addr = ('127.0.0.1', 8000)
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # 创建socket
+
+server_addr = ('127.0.0.1', 8000)       # 链接服务器
 client_sock.connect(server_addr)
 
 while True:
+    msg = input("请输入要发送的消息：")       # 提示用户输入数据
 
-    # 提示用户输入数据
-    msg = input("请输入要发送的消息：")
 
-    # 若用户输入了内容，则发送，否则退出
-    if len(msg) > 0:
+    if len(msg) > 0:        # 若用户输入了内容，则发送，否则退出
         client_sock.send(msg.encode())
-
-        # 接收对方发送过来的数据，最大接收1024个字节
-        recv_data = client_sock.recv(1024)
+        recv_data = client_sock.recv(1024)      # 接收对方发送过来的数据，最大接收1024个字节
         print("收到的消息：%s" % recv_data.decode())
     else:
         break
 
-# 关闭套接字
-client_sock.close()
+client_sock.close()     # 关闭套接字
--- my_py_notes_万物皆对象/网络编程/套接字:通信端点/socket/tcp/tcp_模拟QQ聊天_server.py
+++ my_py_notes_万物皆对象/网络编程/套接字:通信端点/socket/tcp/tcp_模拟QQ聊天_server.py
@@ -9,41 +9,28 @@
 
 import socket
 
-# 创建socket
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 设置socket可以重用地址
-server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # 创建socket
+server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   # 设置socket可以重用地址
 
-# 绑定本地信息
-address = ('', 8000)
+address = ('', 8000)    # 绑定本地信息
 server_sock.bind(address)
 
-# 开启监听
-server_sock.listen(128)
+server_sock.listen(128)     # 开启监听
 
 while True:
-    # 接收客户端的连接请求
-    client_sock, client_addr = server_sock.accept()
-    print("与客户端%s:%s建立了连接" % client_addr)
+    client_sock, client_addr = server_sock.accept()     # 接收客户端的连接请求
+    print("与客户端%s:%s建立了连接" % (client_addr, client_addr))
 
     while True:
-
-        # 接收对方发送过来的数据，最大接收1024个字节
         recv_data = client_sock.recv(1024)
-
-        # 如果接收的数据的长度为0，则意味着客户端关闭了链接
-        if len(recv_data) > 0:
+        if len(recv_data) > 0:                  # 如果接收的数据的长度为0，则意味着客户端关闭了链接
             print("客户端说:%s" % recv_data.decode())
-            # 发送一些数据到客户端
-            msg = input("请输入回复的内容：")
+            msg = input("请输入回复的内容：")      # 发送一些数据到客户端
             client_sock.send(msg.encode())
         else:
             break
 
-    print("客户端%s:%s已下线" % client_addr
-          )
-    # 关闭客户端socket
-    client_sock.close()
+    print("客户端%s:%s已下线" % (client_addr, client_addr))
+    client_sock.close()     # 关闭客户端socket
 
-# 关闭服务器socket
-server_sock.close()
+server_sock.close()     # 关闭服务器socket
