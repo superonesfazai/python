d8c78cce90ec3de8f6d61827bffc6580a205f090
superonesfazai <superonesfazai@gmail.com>
2017-08-19 22:49:40 +0800

    Sat Aug 19 22:49:40 2017

---
+++ python网络数据采集/用爬虫测试网站/play_with_scrapy_module/__init__.py
@@ -0,0 +1,8 @@
+# coding = utf-8
+
+'''
+@author = super_fazai
+@File    : __init__.py.py
+@Time    : 2017/8/19 16:44
+@connect : superonesfazai@gmail.com
+'''
---
+++ python网络数据采集/用爬虫测试网站/play_with_scrapy_module/quote_toscrape_com_demo.py
@@ -0,0 +1,32 @@
+# coding = utf-8
+
+'''
+@author = super_fazai
+@File    : quote_toscrape_com_demo.py
+@Time    : 2017/8/19 15:41
+@connect : superonesfazai@gmail.com
+'''
+
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'http://quotes.toscrape.com/tag/humor/'
+    ]
+
+    def parse(self, response):
+        for quote in response.css(div.quote):
+            yield {
+                'text': quote.css('span.text::text').extract[0],
+                'author': quote.xpath('span/small').extract[0],
+            }
+
+        next_page = response.css('li.next a::attr("href")').extract[0]
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+"""
+测试方式:
+    scrapy runspider quote_toscrape_com_demo.py -o quote.json
+"""
