1e4604aa7d0693887d85de676e8e8969e92baa1f
superonesfazai <superonesfazai@gmail.com>
2017-09-09 16:06:38 +0800

    Sat Sep  9 16:06:38 2017

--- 日常脚本/auto_git_by_afa.py
+++ 日常脚本/auto_git_by_afa.py
@@ -3,16 +3,24 @@
 import os
 import time
 
-def auto_git():
+def auto_git(path):
     # os.popen('cd ~/myFiles/codeDoc/PythonDoc && ls')
     # time.sleep(0.5)
-    path = '~/myFiles/codeDoc/PythonDoc'
     os.popen('cd {} && git add --all'.format(path))
     time.sleep(2)
     os.system('cd {} && git commit -m "{}"'.format(path, time.ctime()))
     time.sleep(2)
     os.system('cd {} && git push -u origin master'.format(path))
-    print('提交成功!!')
+    print(path + '提交成功!!' + '\n\n')
+
+def main():
+    python_path = '~/myFiles/codeDoc/PythonDoc'
+    js_path = '~/myFiles/codeDoc/js_doc'
+    jquery = '~/myFiles/codeDoc/jquery'
+
+    auto_git(python_path)
+    auto_git(js_path)
+    auto_git(jquery)
 
 if __name__ == '__main__':
-    auto_git()
+    main()
