a8513ef6e66d831e3211d9559890fc904a24e9fe
superonesfazai <superonesfazai@gmail.com>
2017-11-21 14:35:50 +0800

    2017-11-21 14:35:50.277759

--- 日常脚本/auto_git_by_afa.py
+++ 日常脚本/auto_git_by_afa.py
@@ -2,34 +2,18 @@
 
 import os
 import time
-import pytz
 import datetime
-import re
 
 def auto_git(path):
     # os.popen('cd ~/myFiles/codeDoc/PythonDoc && ls')
     # time.sleep(0.5)
     print((path+'  正在提交').center(100, '*'))
-    os.popen('cd {} && git add --all'.format(path))
+    os.popen('cd {0} && git add --all'.format(path))
     time.sleep(2)
-    os.system('cd {} && git commit -m "{}"'.format(path, get_shanghai_time()))
+    os.system('cd {0} && git commit -m "{1}"'.format(path, str(datetime.datetime.now())))
     time.sleep(2)
-    os.system('cd {} && git push -u origin master'.format(path))
+    os.system('cd {0} && git push -u origin master'.format(path))
     print((path + ' 提交成功!!').center(100, '*') + '\n')
-
-def get_shanghai_time():
-    '''
-    时区处理，时间处理到上海时间
-    '''
-    tz = pytz.timezone('Asia/Shanghai')  # 创建时区对象
-    now_time = datetime.datetime.now(tz)
-
-    # 处理为精确到秒位，删除时区信息
-    now_time = re.compile(r'\..*').sub('', str(now_time))
-    # 将字符串类型转换为datetime类型
-    now_time = datetime.datetime.strptime(now_time, '%Y-%m-%d %H:%M:%S')
-
-    return str(now_time)
 
 def main():
     python_path = '~/myFiles/codeDoc/PythonDoc'
