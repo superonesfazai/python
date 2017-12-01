c479dd7ba25bd602b1984ce31c48c6c391aaf203
superonesfazai <superonesfazai@gmail.com>
2017-12-01 18:23:11 +0800

    2017-12-01 18:23:11

--- python网络数据采集/用爬虫测试网站/play_with_scrapy_module/my_spiders/电商项目/阿里1688_淘宝_天猫_京东_折800/my_flask_server/just_fuck_run.py
+++ python网络数据采集/用爬虫测试网站/play_with_scrapy_module/my_spiders/电商项目/阿里1688_淘宝_天猫_京东_折800/my_flask_server/just_fuck_run.py
@@ -8,19 +8,19 @@
 '''
 
 import os
-import time
+from time import sleep
 import datetime
 import re
 
 def auto_run(path):
-    print('开始执行秒杀脚本'.center(60, '-'))
+    print('开始执行秒杀脚本'.center(60, '*'))
     os.system('cd {0} && python3 zhe_800_spike.py'.format(path))
     os.system('cd {0} && python3 zhe_800_miaosha_real-times_update.py'.format(path))
     os.system('cd {0} && python3 juanpi_spike.py'.format(path))
     os.system('cd {0} && python3 juanpi_miaosha_real-times_update.py'.format(path))
     os.system('cd {0} && python3 pinduoduo_spike.py'.format(path))
     os.system('cd {0} && python3 pinduoduo_miaosha_real-times_update.py'.format(path))
-    print('脚本执行完毕'.center(60, '-'))
+    print('脚本执行完毕'.center(60, '*'))
 
 def main():
     python_path = '~/myFiles/python/my_flask_server/spike_everything'
