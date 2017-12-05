a7b1e60ca01ab8d6d5390977dbcf351eb3455b8e
superonesfazai <superonesfazai@gmail.com>
2017-12-05 09:36:41 +0800

    2017-12-05 09:36:41

---
+++ python网络数据采集/my_爬虫_进阶_之路/play_with_scrapy_module/my_spiders/pcbody母婴食谱库/tmp_pcbody.py
@@ -0,0 +1,142 @@
+# coding:utf-8
+
+'''
+@author = super_fazai
+@File    : tmp_pcbody.py
+@Time    : 2017/12/3 14:30
+@connect : superonesfazai@gmail.com
+'''
+
+import requests
+import re
+from scrapy.selector import Selector
+import selenium.webdriver.support.ui as ui
+from pprint import pprint
+from selenium import webdriver
+import gc
+from time import sleep
+
+# phantomjs驱动地址
+EXECUTABLE_PATH = '/Users/afa/myFiles/tools/phantomjs-2.1.1-macosx/bin/phantomjs'
+
+def get_body():
+    start_url = 'http://life.pcbaby.com.cn/xms/'
+    all_ul_list = []
+    for index in range(0, 100):
+        if index == 0:
+            next_url = start_url
+        else:
+            next_url = start_url +'index_{0}.html'.format(index)
+        print('待处理的next_url为: ', next_url)
+        response = requests.get(next_url)
+        body = response.content.decode('gbk')
+        # body = re.compile(r'\n').sub('', body)
+        body = re.compile(r'\t').sub('', body)
+        body = re.compile(r'  ').sub('', body)
+        # print(body)
+
+        tmp_ul_list = parse_1(body=body)
+        print(tmp_ul_list)
+        for item in tmp_ul_list:
+            all_ul_list.append(item)
+    print(all_ul_list)
+
+def parse_1(body):
+    # ul.mb30 li
+    ul = Selector(text=body).css('ul.mb30').extract_first()
+    if ul is None:
+        print('ul为空值')
+        return []
+
+    # print(ul)
+    # print(list(Selector(text=ul).css('li').extract()))
+    my_ul = []
+    for item in list(Selector(text=ul).css('li').extract()):
+        li_url = 'http:' + Selector(text=item).css('dd.oh p a::attr("href")').extract_first()
+        # print(li_url)
+        title = Selector(text=item).css('dd.oh p a::text').extract_first()
+        simple_intro = Selector(text=item).css('dd.oh p.aList-intro::text').extract_first()
+        main_img_url = Selector(text=item).css('div.aList-img img::attr("src")').extract_first()
+        if main_img_url is not None:
+            main_img_url = 'http:' + main_img_url
+
+        else:
+            main_img_url = ''
+        content = my_tmp.get_body(url=li_url)
+        # print(content)
+        my_li = {
+            'li_url': li_url,               # 新闻的地址
+            'title': title,                 # 新闻的标题
+            'simple_intro': simple_intro,   # 新闻的简介
+            'main_img_url': main_img_url,   # 新闻的主图
+            'content': content,             # 新闻的内容
+        }
+        print(my_li)
+        my_ul.append(my_li)
+    # print(my_ul)
+    return my_ul
+
+class MyTmp():
+    def init_phantomjs(self):
+        print('--->>>初始化phantomjs驱动中<<<---')
+        cap = webdriver.DesiredCapabilities.PHANTOMJS
+        cap['phantomjs.page.settings.resourceTimeout'] = 1000  # 1秒
+        cap['phantomjs.page.settings.loadImages'] = False
+        cap['phantomjs.page.settings.disk-cache'] = True
+        # cap['phantomjs.page.settings.userAgent'] = HEADERS[randint(0, 34)]  # 随机一个请求头
+        # cap['phantomjs.page.customHeaders.Cookie'] = cookies
+
+        self.driver = webdriver.PhantomJS(executable_path=EXECUTABLE_PATH, desired_capabilities=cap)
+
+        wait = ui.WebDriverWait(self.driver, 15)  # 显示等待n秒, 每过0.5检查一次页面是否加载完毕
+        print('------->>>初始化完毕<<<-------')
+
+    def get_body(self, url):
+        self.driver.set_page_load_timeout(10)  # 设置成10秒避免数据出错
+
+        try:
+            self.driver.get(url)
+            self.driver.implicitly_wait(15)
+            body = self.driver.page_source
+            body = re.compile(r'\n').sub('', body)
+            body = re.compile(r'\t').sub('', body)
+            body = re.compile(r'  ').sub('', body)
+            # print(body)
+
+        except Exception as e:  # 如果超时, 终止加载并继续后续操作
+            print('-->>time out after 10 seconds when loading page')
+            self.driver.execute_script('window.stop()')  # 当页面加载时间超过设定时间，通过执行Javascript来stop加载，即可执行后续动作
+            body = ''
+
+        # tmp_body = Selector(text=body).css('div.artText').extract_first()
+        tmp_body = re.compile(r'<div class="artText">(.*)<div class="artNav">').findall(body)
+        if tmp_body != []:
+            tmp_body = '<div class="artText">' + tmp_body[0]
+            tmp_body = re.compile(r'src="//www1.pcbaby.com.cn/images/blank.gif" #').sub(r'', tmp_body)
+            tmp_body = re.compile(r'图片来源：太平洋亲子网').sub(r'', tmp_body)
+            tmp_body = re.compile(r'<p>　　打开淘宝APP，搜索“@太平洋亲子”，<em style="color: #f44">关注</em>后抽母婴大奖。</p>').sub('', tmp_body)
+            tmp_body = re.compile(r'src=\"').sub('src=\"http:', tmp_body)
+            # print(tmp_body)
+            return tmp_body
+        else:
+            print('tmp_body为空值')
+            return ''
+
+    def __del__(self):
+        try:
+            self.driver.quit()
+        except:
+            pass
+        gc.collect()
+
+my_tmp = MyTmp()
+my_tmp.init_phantomjs()
+
+if __name__ == '__main__':
+    get_body()
+    try:
+        del my_tmp
+    except:
+        pass
+    gc.collect()
+
---
+++ python网络数据采集/my_爬虫_进阶_之路/play_with_scrapy_module/my_spiders/电商项目/阿里1688_淘宝_天猫_京东_折800_卷皮_拼多多/my_flask_server/well_recommend/aiguangjie.py
@@ -0,0 +1,116 @@
+# coding:utf-8
+
+'''
+@author = super_fazai
+@File    : aiguangjie.py
+@Time    : 2017/12/4 20:57
+@connect : superonesfazai@gmail.com
+'''
+
+import sys
+sys.path.append('..')
+
+import requests
+import re
+import json
+import gc, os
+from random import randint
+from settings import HEADERS
+from settings import IS_BACKGROUND_RUNNING
+
+class AiGuangJie(object):
+    def __init__(self):
+        # * requests模拟ajax请求 *
+        self.headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/json,application/xml;q=0.9,*/*;q=0.8',
+            # 'Accept-Encoding:': 'gzip',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'guang.taobao.com',
+            'User-Agent': HEADERS[randint(0, 34)],      # 随机一个请求头
+            # 'X-Requested-With': 'XMLHttpRequest',
+            'Referer': 'https://guang.taobao.com/',  # 必须的参数
+        }
+
+    def get_goods_info_list(self):
+        '''
+        测试二: 模拟ajax爱逛街pc端接口
+        '''
+        tmp_url = 'https://guang.taobao.com/street/ajax/get_guang_list.json?_input_charset=utf-8&cpage=1&start=1&_tb_token_=bd731a300e5e&_ksTS=1512382359042_177'
+        response2 = requests.get(tmp_url, headers=self.headers)
+        body2 = response2.content.decode('utf-8')
+        print(body2)
+
+    def __del__(self):
+        gc.collect()
+
+def daemon_init(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+    '''
+    杀掉父进程，独立子进程
+    :param stdin:
+    :param stdout:
+    :param stderr:
+    :return:
+    '''
+    sys.stdin = open(stdin, 'r')
+    sys.stdout = open(stdout, 'a+')
+    sys.stderr = open(stderr, 'a+')
+    try:
+        pid = os.fork()
+        if pid > 0:     # 父进程
+            os._exit(0)
+    except OSError as e:
+        sys.stderr.write("first fork failed!!" + e.strerror)
+        os._exit(1)
+
+    # 子进程， 由于父进程已经退出，所以子进程变为孤儿进程，由init收养
+    '''setsid使子进程成为新的会话首进程，和进程组的组长，与原来的进程组、控制终端和登录会话脱离。'''
+    os.setsid()
+    '''防止在类似于临时挂载的文件系统下运行，例如/mnt文件夹下，这样守护进程一旦运行，临时挂载的文件系统就无法卸载了，这里我们推荐把当前工作目录切换到根目录下'''
+    os.chdir("/")
+    '''设置用户创建文件的默认权限，设置的是权限“补码”，这里将文件权限掩码设为0，使得用户创建的文件具有最大的权限。否则，默认权限是从父进程继承得来的'''
+    os.umask(0)
+
+    try:
+        pid = os.fork()  # 第二次进行fork,为了防止会话首进程意外获得控制终端
+        if pid > 0:
+            os._exit(0)  # 父进程退出
+    except OSError as e:
+        sys.stderr.write("second fork failed!!" + e.strerror)
+        os._exit(1)
+
+    # 孙进程
+    #   for i in range(3, 64):  # 关闭所有可能打开的不需要的文件，UNP中这样处理，但是发现在python中实现不需要。
+    #       os.close(i)
+    sys.stdout.write("Daemon has been created! with pid: %d\n" % os.getpid())
+    sys.stdout.flush()  # 由于这里我们使用的是标准IO，这里应该是行缓冲或全缓冲，因此要调用flush，从内存中刷入日志文件。
+
+def just_fuck_run():
+    while True:
+        print('一次大更新即将开始'.center(30, '-'))
+        tmp = AiGuangJie()
+        tmp.get_goods_info_list()
+        try:
+            del tmp
+        except:
+            pass
+        gc.collect()
+        print('一次大更新完毕'.center(30, '-'))
+
+def main():
+    '''
+    这里的思想是将其转换为孤儿进程，然后在后台运行
+    :return:
+    '''
+    print('========主函数开始========')  # 在调用daemon_init函数前是可以使用print到标准输出的，调用之后就要用把提示信息通过stdout发送到日志系统中了
+    daemon_init()  # 调用之后，你的程序已经成为了一个守护进程，可以执行自己的程序入口了
+    print('--->>>| 孤儿进程成功被init回收成为单独进程!')
+    # time.sleep(10)  # daemon化自己的程序之后，sleep 10秒，模拟阻塞
+    just_fuck_run()
+
+if __name__ == '__main__':
+    if IS_BACKGROUND_RUNNING:
+        main()
+    else:
+        just_fuck_run()
