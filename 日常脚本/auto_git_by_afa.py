dda27f0e28a101917bfb4e13b213e1a535ef7376
superonesfazai <superonesfazai@gmail.com>
2018-07-20 21:50:36 +0800

    2018-07-20 21:50:36

--- 日常脚本/auto_git_by_afa.py
+++ 日常脚本/auto_git_by_afa.py
@@ -3,7 +3,9 @@
 from fzutils.linux_utils import auto_git
 
 def main():
+    # home_linux
     python_path = '/home/afa/myFiles/codeDoc/pythonDoc/python'
+    # cp_mac
 
     auto_git(python_path)
     print(' Money is on the way! '.center(100, '*'))
