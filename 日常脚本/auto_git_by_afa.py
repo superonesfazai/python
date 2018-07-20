951ed8780d91e1a0db602ef1daeabca6daef388c
superonesfazai <superonesfazai@gmail.com>
2018-07-20 21:38:49 +0800

    2018-07-20 21:38:49

--- 日常脚本/auto_git_by_afa.py
+++ 日常脚本/auto_git_by_afa.py
@@ -1,33 +1,11 @@
 # coding=utf-8
 
-import os
-import time
-import datetime
-import re
-
-def auto_git(path):
-    # os.popen('cd ~/myFiles/codeDoc/PythonDoc && ls')
-    # time.sleep(0.5)
-    print((path+'  正在提交').center(100, '*'))
-    os.popen('cd {0} && git add --all'.format(path))
-    time.sleep(2)
-    now_time = str(datetime.datetime.now())
-    now_time = str(re.compile(r'\..*').sub('', now_time))
-    os.system('cd {0} && git commit -m "{1}"'.format(path, now_time))
-    time.sleep(2)
-    os.system('cd {0} && git push -u origin master'.format(path))
-    print((path + ' 提交成功!!').center(100, '*') + '\n')
+from fzutils.linux_utils import auto_git
 
 def main():
-    python_path = '~/myFiles/codeDoc/PythonDoc'
-    js_path = '~/myFiles/codeDoc/js_doc'
-    jquery_path = '~/myFiles/codeDoc/jquery'
-    html_path = '~/myFiles/codeDoc/html_doc'
- 
+    python_path = '/home/afa/myFiles/codeDoc/pythonDoc/python'
+
     auto_git(python_path)
-    auto_git(html_path)
-    auto_git(js_path)
-    auto_git(jquery_path)
     print(' Money is on the way! '.center(100, '*'))
 
 if __name__ == '__main__':
