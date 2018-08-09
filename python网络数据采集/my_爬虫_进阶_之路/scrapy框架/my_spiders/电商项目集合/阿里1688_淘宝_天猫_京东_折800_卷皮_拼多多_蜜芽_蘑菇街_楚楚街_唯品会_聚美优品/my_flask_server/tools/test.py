eb640334035af4cc3c122b4274c6f074bbdb2d6d
afahost <superonesfazai@gmail.com>
2018-08-09 15:47:02 +0800

    2018-08-09 15:47:01

--- python网络数据采集/my_爬虫_进阶_之路/scrapy框架/my_spiders/电商项目集合/阿里1688_淘宝_天猫_京东_折800_卷皮_拼多多_蜜芽_蘑菇街_楚楚街_唯品会_聚美优品/my_flask_server/tools/test.py
+++ python网络数据采集/my_爬虫_进阶_之路/scrapy框架/my_spiders/电商项目集合/阿里1688_淘宝_天猫_京东_折800_卷皮_拼多多_蜜芽_蘑菇街_楚楚街_唯品会_聚美优品/my_flask_server/tools/test.py
@@ -16,3 +16,27 @@ from fzutils.spider.fz_requests import MyRequests
 # var e = this.props.el;
 # arguments里面
 
+with open('../templates/kaola_spider_2_show.html', 'r') as f:
+    text_1 = f.read()
+
+print(text_1)
+
+with open('../templates/yanxuan_spider_2_show.html', 'r') as f:
+    text_2 = f.read()
+
+# print(text_1)
+
+text1 = text_1.splitlines()
+text2 = text_2.splitlines()
+
+# d = difflib.Differ()
+# diff = d.compare(text1, text2)
+# print('\n'.join(list(diff)))
+
+# 替换成
+import difflib
+d = difflib.HtmlDiff()
+d_html_code = d.make_file(text1, text2)
+
+with open('diff_html.html', 'wb') as f:
+    f.write(d_html_code.encode('utf-8'))
